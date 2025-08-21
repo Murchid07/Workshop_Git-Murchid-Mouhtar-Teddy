a89e39bdeb47d7c010784b6629cf59798a8eaf76
Marc teddy <teddy.kpatinkpo@epitech.eu>
2025-08-21 16:52:14 +0100

    my_putchar

---
+++ mob.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2025
+** workshop
+** File description:
+** mob.c
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void my_putchar(char c)
+{
+    write(1, &c, 1);
+}
